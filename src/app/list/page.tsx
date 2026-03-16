d63613ab3dcfb10ae7ad473fea88066e8f2b4a1e
gyeongtaggim865-hue <gyeongtaggim865@gmail.com>
2026-03-16 15:34:54 +0900

    flex 적용 및 backend 데이터 가져와서 브라우저 랜더링

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,3 +1,30 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 export default function Home() {
-    return <div>여기는 list 페이지입니다.</div>;
+
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:8080/api/v1/posts")
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                setPosts(data)
+            })
+    }, []);
+
+      return (
+      <div className="flex flex-col gap-4">
+        <h1>글 목록</h1>
+        <ul>
+        {posts.map((post: {id:number, title:string}) => (
+          <li key={post.id}>
+            {post.id} : {post.title}
+          </li>
+        ))}
+        </ul>
+      </div>
+      );
 }
